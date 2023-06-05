2f20ee937ae3839644bccf5bc406f1dfff57ff24
CaptainHarryChen <captainharrychen@qq.com>
2023-06-05 06:51:44 +0000

    完成或运算短路

--- src/ast/exp_ast.hpp
+++ src/ast/exp_ast.hpp
@@ -425,7 +425,7 @@ public:
 
 class LAndExpAST : public BaseAST
 {
-    void *make_bool_koopa(koopa_raw_slice_t parent, koopa_raw_value_t exp) const
+    koopa_raw_value_data *make_bool_koopa(koopa_raw_slice_t parent, koopa_raw_value_t exp) const
     {
         NumberAST zero(0);
         koopa_raw_value_data *res = new koopa_raw_value_data();
@@ -475,18 +475,68 @@ public:
             res = (koopa_raw_value_data *)leftExp->build_koopa_values(parent);
             break;
         case Op:
+            koopa_raw_value_data *temp_var = new koopa_raw_value_data();
+            koopa_raw_slice_t child_used_by = make_koopa_rs_single_element(temp_var, KOOPA_RSIK_VALUE);
+            temp_var->ty = make_int_pointer_type();
+            temp_var->name = new_char_arr("%temp");
+            temp_var->used_by = parent;
+            temp_var->kind.tag = KOOPA_RVT_ALLOC;
+            block_maintainer.AddInst(temp_var);
+            
+            koopa_raw_value_data *temp_store = new koopa_raw_value_data();
+            child_used_by = make_koopa_rs_single_element(temp_store, KOOPA_RSIK_VALUE);
+            temp_store->ty = simple_koopa_raw_type_kind(KOOPA_RTT_UNIT);
+            temp_store->name = nullptr;
+            temp_store->used_by = empty_koopa_rs();
+            temp_store->kind.tag = KOOPA_RVT_STORE;
+            temp_store->kind.data.store.dest = temp_var;
+            temp_store->kind.data.store.value = (koopa_raw_value_t)zero->build_koopa_values(child_used_by);
+            block_maintainer.AddInst(temp_store);
+
+            koopa_raw_value_data *br = new koopa_raw_value_data();
+            child_used_by = make_koopa_rs_single_element(br, KOOPA_RSIK_VALUE);
+            br->ty = simple_koopa_raw_type_kind(KOOPA_RTT_UNIT);
+            br->name = nullptr;
+            br->used_by = parent;
+            br->kind.tag = KOOPA_RVT_BRANCH;
+            br->kind.data.branch.cond = make_bool_koopa(child_used_by, (koopa_raw_value_t)leftExp->build_koopa_values(child_used_by));
+            koopa_raw_basic_block_data_t *true_block = new koopa_raw_basic_block_data_t();
+            koopa_raw_basic_block_data_t *end_block = new koopa_raw_basic_block_data_t();
+            br->kind.data.branch.true_bb = true_block;
+            br->kind.data.branch.false_bb = end_block;
+            br->kind.data.branch.true_args = empty_koopa_rs(KOOPA_RSIK_VALUE);
+            br->kind.data.branch.false_args = empty_koopa_rs(KOOPA_RSIK_VALUE);
+            block_maintainer.AddInst(br);
+
+            true_block->name = new_char_arr("%true");
+            true_block->params = empty_koopa_rs(KOOPA_RSIK_VALUE);
+            true_block->used_by = empty_koopa_rs(KOOPA_RSIK_VALUE);
+            block_maintainer.AddNewBasicBlock(true_block);
+            
+            koopa_raw_value_data *b_store = new koopa_raw_value_data();
+            child_used_by = make_koopa_rs_single_element(temp_store, KOOPA_RSIK_VALUE);
+            b_store->ty = simple_koopa_raw_type_kind(KOOPA_RTT_UNIT);
+            b_store->name = nullptr;
+            b_store->used_by = empty_koopa_rs();
+            b_store->kind.tag = KOOPA_RVT_STORE;
+            b_store->kind.data.store.dest = temp_var;
+            b_store->kind.data.store.value = make_bool_koopa(child_used_by, (koopa_raw_value_t)rightExp->build_koopa_values(child_used_by));
+            block_maintainer.AddInst(b_store);
+            block_maintainer.AddInst(JumpInst(end_block));
+
+            end_block->name = new_char_arr("%end");
+            end_block->params = empty_koopa_rs(KOOPA_RSIK_VALUE);
+            end_block->used_by = empty_koopa_rs(KOOPA_RSIK_VALUE);
+            block_maintainer.AddNewBasicBlock(end_block);
+
             res = new koopa_raw_value_data();
-            koopa_raw_slice_t child_used_by = make_koopa_rs_single_element(res, KOOPA_RSIK_VALUE);
             res->ty = simple_koopa_raw_type_kind(KOOPA_RTT_INT32);
             res->name = nullptr;
             res->used_by = parent;
-            res->kind.tag = KOOPA_RVT_BINARY;
-            auto &binary = res->kind.data.binary;
-            if (op == "&&")
-                binary.op = KOOPA_RBO_AND;
-            binary.lhs = (koopa_raw_value_t)make_bool_koopa(child_used_by, (koopa_raw_value_t)leftExp->build_koopa_values(child_used_by));
-            binary.rhs = (koopa_raw_value_t)make_bool_koopa(child_used_by, (koopa_raw_value_t)rightExp->build_koopa_values(child_used_by));
+            res->kind.tag = KOOPA_RVT_LOAD;
+            res->kind.data.load.src = temp_var;
             block_maintainer.AddInst(res);
+
             break;
         }
         return res;
--- test/hello.c
+++ test/hello.c
@@ -1,7 +1,3 @@
 int main() {
-  int a = 2;
-  if (a) {
-    a = a + 1;
-  } else a = 0;  // 在实际写 C/C++ 程序的时候别这样, 建议 if 的分支全部带大括号
-  return a;
+  return 1 && 0;
 }
